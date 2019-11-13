b0d3eeb7ae21bdf4f0e1c1bad4f14b88fb6a52c1
abbieolson <abbiefayeolson@gmail.com>
2019-11-13 11:46:51 -0800

    updates

---
+++ olson_deduper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+# import argparse
+#
+# parser = argparse.ArgumentParser(description="Program for demultiplexing")
+#
+# parser.add_argument("-f", "--file", help="", required=True, type=str)
+# parser.add_argument("-p", "--paired", help="", required=True, type=str)
+# parser.add_argument("-u", "--umi", help="", required=True, type=str)
+# parser.add_argument("-h", "--help", help="", required=True, type=str)
+#
+# args = parser.parse_args()
+#
+#
+# FILE = args.file
+# PAIRED = args.paired
+# UMI = args.umi
+# HELP = args.help
+
+
+# read in the BIOO barcode indices file and initialize a dictioary
+
+barcode_fi
